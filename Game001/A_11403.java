15ef828fa3f3c0f22b34beb4e031a96e7fa7a0cb
seungki.noh <snky919@gmail.com>
2016-05-12 23:40:41 +0900

    FastScanner 소스 추가

--- Game001/A_11403.java
+++ Game001/A_11403.java
@@ -1,6 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class A_11403 {
     public static void main(String[] args) {
-    	
+    	FastScanner sc = new FastScanner();
+    }
+}
+
+class FastScanner {
+    
+    BufferedReader br;
+    StringTokenizer st;
+     
+    public FastScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+     
+    String next() {
+        while(st == null || !st.hasMoreTokens()){
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+     
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    
+    long nextLong() {
+    	return Long.parseLong(next());
+    }
+     
+    String nextLine() {
+        String line = "";
+        try {
+            line = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line ;
     }
 }
